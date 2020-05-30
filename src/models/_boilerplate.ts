81a7ca5a47b1ce76aea88a79b134e788d389c07d
joaohenriquef <joaohenriquef@poli.ufrj.br>
2020-05-29 23:38:21 -0300

    Add draft boilerplate for classes

---
+++ src/models/_boilerplate.ts
@@ -0,0 +1,15 @@
+// Draft file, will be deleted
+type Props = {
+  id: number,
+  playerIds: Array<number>,
+  hostPlayerId: number,
+  state: any,
+}
+
+export class Player {
+  constructor(props: Props) {
+    for (let key in props) {
+      this[key] = props[key];
+    }
+  }
+}
